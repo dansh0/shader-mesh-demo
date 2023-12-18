0ca5aa30cd2d4f4c9dfe3d512b81f502a0dc1401
Dan <dshores16@gmail.com>
2023-12-18 23:06:54 +0700

    some lilGUI

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from './jsm/controls/OrbitControls.js'
 import { RoundedBoxGeometry } from './jsm/geometries/RoundedBoxGeometry.js'
+import { Reflector } from './jsm/objects/Reflector.js';
 import Stats from './jsm/libs/stats.module.js'
 import { GUI } from './jsm/libs/lil-gui.module.min.js'
 
@@ -45,18 +46,95 @@ const frag = `
     uniform float uLightTheta;
     uniform float uLightPhi;
     uniform vec3 uLightCol;
+    uniform int uGeoType;
+    uniform float uFillFactor;
 
     // Declare Parameters
     float ambiStrength = 0.4; // Ambient light strength
     float diffStength = 0.4; // Diffuse light strength
     float specStrength = 0.2; // Specular light strength
     float specPow = 4.0; // Specular light power (spread)
-    float gyroidFactor = 0.8; // Factor for shape of gyroid
+
+    // GEOMETRIES
 
     // Gyroid SDF
     float distGyroidBeam(vec3 point, float scale) {
         point *= scale;
-        return (dot(sin(point), cos(point.zxy))) + gyroidFactor;
+        return (dot(sin(point), cos(point.zxy))) + (1.5-uFillFactor*3.0);
+    }
+
+    float distGyroidSurface(vec3 point, float scale) {
+        point *= scale;
+        return abs(dot(sin(point), cos(point.zxy))) - uFillFactor*1.5;
+    }
+
+    // Shwarz P SDF
+    float distSchwarzP(vec3 point, float scale) {
+        point *= scale;
+        return abs(dot(vec3(1.), cos(point))) - uFillFactor*1.5;
+    }
+
+    // 3D repition from HG_SDF https://mercury.sexy/hg_sdf/
+    void pMod3(inout vec3 p, vec3 size) {
+        p = mod(p + size*0.5, size) - size*0.5;
+    }
+
+    // Octet SDF
+    float distBeam(vec3 point, vec3 normal, float radius) {
+        return length(point - dot(point, normal) * normal) - radius;
+    }
+
+    float distOctet(vec3 position, float scale) {
+        float spacing = 10.0/scale;
+        float beamRadius = uFillFactor*2.0;
+        
+        vec3 positionShiftXZ = position + vec3(spacing/2.0, 0.0, spacing/2.0);
+        vec3 positionShiftXY = position + vec3(spacing/2.0, spacing/2.0, 0.0);
+        
+        pMod3( position, vec3(spacing));
+        position.x = abs(position.x);
+        position.y = abs(position.y);
+        position.z = abs(position.z);
+        if (position.y > position.x) { position.xy = position.yx; } 
+        if (position.z > position.y) { position.yz = position.zy; } 
+        
+        float beamPlanar = distBeam(position, normalize(vec3(1.0,1.0,0.0)), beamRadius); 
+        
+        pMod3( positionShiftXZ, vec3(spacing));
+        positionShiftXZ.x = abs(positionShiftXZ.x);
+        positionShiftXZ.y = abs(positionShiftXZ.y);
+        positionShiftXZ.z = abs(positionShiftXZ.z);
+        if (positionShiftXZ.y > positionShiftXZ.x) { positionShiftXZ.xy = positionShiftXZ.yx; } 
+        if (positionShiftXZ.z > positionShiftXZ.y) { positionShiftXZ.yz = positionShiftXZ.zy; } 
+
+        float beamAngles = distBeam(positionShiftXZ, normalize(vec3(1.0,1.0,0.0)), beamRadius);
+        
+        
+        pMod3( positionShiftXY, vec3(spacing));
+        positionShiftXY = abs(positionShiftXY);
+        float beamSection = distBeam(positionShiftXY, normalize(vec3(1.0,0.0,1.0)), beamRadius);
+        
+        return min(min(beamPlanar, beamAngles), beamSection);
+    }
+
+    // Select Geometry Type
+    float distFill(vec3 position, float scale) {
+        float dist;
+        switch(uGeoType) {
+            case 0:
+                dist = distGyroidBeam(position, scale);
+                break;
+            case 1:
+                dist = distGyroidSurface(position, scale);
+                break;
+            case 2:
+                dist = distSchwarzP(position, scale);
+                break;
+            case 3:
+                dist = distOctet(position, scale);
+                break;
+        }
+        return dist;
     }
 
     // Fixed Step Ray Marcher
@@ -64,7 +142,7 @@ const frag = `
         float dist = near;
         position += near * direction;
         for (int iStep=0; iStep<MAX_STEPS; iStep++) {
-            float distToGeo = distGyroidBeam(position, scale);
+            float distToGeo = distFill(position, scale);
             if (distToGeo > MIN_DIST && dist < MAX_DIST && dist < far) {
                 position += uStepSize * direction;
                 dist += uStepSize;
@@ -76,11 +154,11 @@ const frag = `
     }
 
     // Gyroid Beam Gradient (For Normal)
-    vec3 tpmsGradBeam(vec3 position, float gyroidFactor, float scale) {
+    vec3 tpmsGradBeam(vec3 position, float scale) {
         vec3 change;
-        change.x = (distGyroidBeam( position + vec3(NORM_EPS, 0, 0), scale) - distGyroidBeam( position - vec3(NORM_EPS, 0, 0), scale));
-        change.y = (distGyroidBeam( position + vec3(0, NORM_EPS, 0), scale) - distGyroidBeam( position - vec3(0, NORM_EPS, 0), scale)); 
-        change.z = (distGyroidBeam( position + vec3(0, 0, NORM_EPS), scale) - distGyroidBeam( position - vec3(0, 0, NORM_EPS), scale)); 
+        change.x = (distFill( position + vec3(NORM_EPS, 0, 0), scale) - distFill( position - vec3(NORM_EPS, 0, 0), scale));
+        change.y = (distFill( position + vec3(0, NORM_EPS, 0), scale) - distFill( position - vec3(0, NORM_EPS, 0), scale)); 
+        change.z = (distFill( position + vec3(0, 0, NORM_EPS), scale) - distFill( position - vec3(0, 0, NORM_EPS), scale)); 
         return normalize( change );
     }
 
@@ -105,6 +183,73 @@ const frag = `
         const vec4 bitShifts = vec4(1.0 / (256.0 * 256.0 * 256.0), 1.0 / (256.0 * 256.0), 1.0 / 256.0, 1.0);        float depth = dot(color, bitShifts);
         return depth;
     }
+
+    // 3D Simplex Noise
+    // From: https://www.shadertoy.com/view/XsX3zB
+
+    /* discontinuous pseudorandom uniformly distributed in [-0.5, +0.5]^3 */
+    vec3 random3(vec3 c) {
+        float j = 4096.0*sin(dot(c,vec3(17.0, 59.4, 15.0)));
+        vec3 r;
+        r.z = fract(512.0*j);
+        j *= .125;
+        r.x = fract(512.0*j);
+        j *= .125;
+        r.y = fract(512.0*j);
+        return r-0.5;
+    }
+
+    /* skew constants for 3d simplex functions */
+    const float F3 =  0.3333333;
+    const float G3 =  0.1666667;
+
+    /* 3d simplex noise */
+    float simplex3d(vec3 p) {
+        /* 1. find current tetrahedron T and it's four vertices */
+        /* s, s+i1, s+i2, s+1.0 - absolute skewed (integer) coordinates of T vertices */
+        /* x, x1, x2, x3 - unskewed coordinates of p relative to each of T vertices*/
+        
+        /* calculate s and x */
+        vec3 s = floor(p + dot(p, vec3(F3)));
+        vec3 x = p - s + dot(s, vec3(G3));
+        
+        /* calculate i1 and i2 */
+        vec3 e = step(vec3(0.0), x - x.yzx);
+        vec3 i1 = e*(1.0 - e.zxy);
+        vec3 i2 = 1.0 - e.zxy*(1.0 - e);
+            
+        /* x1, x2, x3 */
+        vec3 x1 = x - i1 + G3;
+        vec3 x2 = x - i2 + 2.0*G3;
+        vec3 x3 = x - 1.0 + 3.0*G3;
+        
+        /* 2. find four surflets and store them in d */
+        vec4 w, d;
+        
+        /* calculate surflet weights */
+        w.x = dot(x, x);
+        w.y = dot(x1, x1);
+        w.z = dot(x2, x2);
+        w.w = dot(x3, x3);
+        
+        /* w fades from 0.6 at the center of the surflet to 0.0 at the margin */
+        w = max(0.6 - w, 0.0);
+        
+        /* calculate surflet components */
+        d.x = dot(random3(s), x);
+        d.y = dot(random3(s + i1), x1);
+        d.z = dot(random3(s + i2), x2);
+        d.w = dot(random3(s + 1.0), x3);
+        
+        /* multiply d by w^4 */
+        w *= w;
+        w *= w;
+        d *= w;
+        
+        /* 3. return the sum of the four surflets */
+        return dot(d, vec4(52.0));
+    }
+    // End of 3D Simplex Noise Segment
 
     void main() {
 
@@ -141,14 +286,24 @@ const frag = `
                 normal = vNormal;
             } else {
                 // Normal of Gyroid
-                normal = tpmsGradBeam(objPos, gyroidFactor, scale);
+                normal = tpmsGradBeam(objPos, scale);
             }
 
+            // Noise
+            vec3 longPos = vec3(objPos.x, objPos.y*2.0, objPos.z);
+            float noiseLarge = 1.0 - 0.3*simplex3d(objPos*0.1);
+            float noiseSmall = 1.0 - 0.2*simplex3d(longPos*0.25);
+
+
             // Adjust Light Position
-            float lRadius = 25.0;
+            float lRadius = 15.0;
             float lPhi = uLightPhi;
             float lTheta = uLightTheta;
             vec3 lightPos = vec3(lRadius*sin(lPhi)*cos(lTheta), lRadius*cos(lPhi), lRadius*sin(lPhi)*sin(lTheta));
+            
+            // Dist From Light
+            float lightDist = length(lightPos-objPos);
+            float cheapAO = min(1.0, 10.0/(lightDist)); 
 
             // Ambient Lighting
             vec3 ambiLight = vec3(1.) * ambiStrength;
@@ -164,7 +319,9 @@ const frag = `
             
             // Phong Combined Lighting
             vec3 combLight = ambiLight + diffLight + specLight;
-            col = combLight * uColor;
+
+            // Mix It All Up!
+            col = combLight * uColor * noiseLarge * noiseSmall * cheapAO;
             
         }
         
@@ -180,7 +337,7 @@ let renderer, camera, controls, stats
 
 // Calc Light Position
 const lightPosition = (phi, theta) => {
-    let radius = 25.0;
+    let radius = 15.0;
     return [
         radius*Math.sin(phi)*Math.cos(theta), 
         radius*Math.cos(phi),
@@ -228,14 +385,22 @@ const Init = () => {
 
     // Model
     let meshConfigs = {
-        'geometry': 'sphere',
-        'lightColor': [1.0, 1.0, 1.0]
+        'geometry': 'box',
+        'lightColor': [1.0, 1.0, 1.0],
+        'lightHue': 1.0
     }
+
+    const Torus = (radius, tube, radSegs, tubeSegs) => {
+        let newTorus = new THREE.TorusGeometry(radius, tube, radSegs, tubeSegs)
+        newTorus.rotateX(Math.PI/2)
+        return newTorus
+    }
+
     const geometries = {
         box: new RoundedBoxGeometry(20, 20, 20, 10, 5),
         flatBox: new RoundedBoxGeometry(2, 20, 20, 10, 5),
         sphere: new THREE.SphereGeometry(10),
-        torus: new THREE.TorusGeometry(10, 4, 16, 100),
+        torus: Torus(10, 4, 16, 100),
     }
     let geometry = geometries[meshConfigs.geometry]
 
@@ -260,7 +425,9 @@ const Init = () => {
         'cellSize': 4,
         'lightTheta': Math.PI/4,
         'lightPhi': Math.PI/3,
-        'color': [0.8,0.8,0.8]
+        'color': [0.8,0.8,0.8],
+        'geoType': 0,
+        'fillFactor': 0.25
     }
     const material = new THREE.ShaderMaterial({
         uniforms: {
@@ -276,7 +443,9 @@ const Init = () => {
             'uColor': { 'value': shaderConfigs.color },
             'uLightTheta': { 'value': shaderConfigs.lightTheta },
             'uLightPhi': { 'value': shaderConfigs.lightPhi },
-            'uLightCol': { 'value': meshConfigs.lightColor }
+            'uLightCol': { 'value': meshConfigs.lightColor },
+            'uGeoType': { 'value': shaderConfigs.geoType },
+            'uFillFactor': { 'value': shaderConfigs.fillFactor }
         },
         vertexShader: vert,
         fragmentShader: frag
@@ -320,6 +489,10 @@ const Init = () => {
             camera.aspect = width/height
             camera.updateProjectionMatrix()
             material.uniforms.uCameraSize.value = new THREE.Vector2(width/dpmm, height/dpmm)
+            groundMirror.getRenderTarget().setSize(
+                window.innerWidth * window.devicePixelRatio,
+                window.innerHeight * window.devicePixelRatio
+            );
             renderer.setSize(width, height)
             render()
         },
@@ -339,10 +512,13 @@ const Init = () => {
         depthMeshFront.geometry = geometries[value]
         animate()
     })
-    sceneFolder.addColor({lightColor:meshConfigs.lightColor}, 'lightColor').onChange( value => {
-        material.uniforms.uLightCol.value = value
-        bulbMat.emissive = new THREE.Color(value[0], value[1], value[2])
-        console.log(bulbMat)
+    sceneFolder.add(meshConfigs, 'lightHue', 0.0, 1.0).onChange( value => {
+        let color = new THREE.Color("hsl("+value*255+", 100%, 50%)")
+        console.log(color)
+        bulbMat.emissive = color
+        let rgb = color.toArray();
+        console.log(rgb)
+        material.uniforms.uLightCol.value = rgb
         animate()
     })
     sceneFolder.add(shaderConfigs, 'lightTheta', 0, Math.PI*2).onChange( value => {
@@ -364,6 +540,14 @@ const Init = () => {
     })
     shaderFolder.add(shaderConfigs, 'cellSize', 0.5, 10).onChange( value => { 
         material.uniforms.uCellSize.value = value 
+        animate()
+    })
+    shaderFolder.add(shaderConfigs, 'fillFactor', 0.1, 1).onChange( value => { 
+        material.uniforms.uFillFactor.value = value 
+        animate()
+    })
+    shaderFolder.add(shaderConfigs, 'geoType', {'gyroidBeam': 0, 'gyroidSurface': 1, 'schwarzP': 2, 'octet': 3}).onChange( value => { 
+        material.uniforms.uGeoType.value = value 
         animate()
     })
     shaderFolder.addColor({color:shaderConfigs.color}, 'color').onChange( value => {
@@ -404,4 +588,3 @@ function render() {
 // Start!
 Init()
 animate()
-// setTimeout(render, 100)
